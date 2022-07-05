c49eee16a0544a76b3e6940d906cd6877b8956b0
Inpyo Song <rihoto@gmail.com>
2022-07-06 03:09:20 +0900

    Add : crawling_game_review.py for crawling reviews

---
+++ crawling_game_review.py
@@ -0,0 +1,62 @@
+import requests
+import time
+import pandas as pd
+
+def get_reviews(appid, params={'json': 1}):
+    url = 'https://store.steampowered.com/appreviews/'
+    response = requests.get(url=url + str(appid), params=params, headers={'User-Agent': 'Mozilla/5.0'})
+    return response.json()
+
+def get_n_reviews(appid, n=100):
+    reviews = []
+    cursor = '*'
+    params = {
+        'json': 1,
+        'filter': 'all',
+        'language': 'koreana',
+        'day_range': 9223372036854775807,
+        'review_type': 'all',
+        'purchase_type': 'all'
+    }
+
+    while n > 0:
+        time.sleep(2)
+        params['cursor'] = cursor.encode()
+        params['num_per_page'] = min(100, n)
+        n -= 100
+
+        response = get_reviews(appid, params)
+        cursor = response['cursor']
+        reviews += response['reviews']
+
+        if len(response['reviews']) < 100: break
+
+    return reviews
+
+data = pd.read_csv('steam_games_information.csv')
+game = data['game']
+appid = data['appid']
+
+review_df = pd.DataFrame() # 모든 게임들의 리뷰를 담을 DF
+
+for i in range(len(data['appid'])):
+    df_reviews = []
+
+    reviews = get_n_reviews(appid[i], n=100)
+    
+    # 리뷰가 100개가 되지 않는 경우를 방지하기 위해 try 사용
+    for j in range(100):
+        try:
+            df_reviews.append(reviews[j]['review'])
+        except:
+            pass
+
+    df = pd.DataFrame({'reviews': df_reviews})
+    
+    # 리뷰 수에 맞추어 게임 이름과 id를 넣어준다.
+    df['game'] = data['game'][i]
+    df['appid'] = data['appid'][i]
+
+    review_df = pd.concat([review_df, df], ignore_index=True)
+
+review_df.to_csv('steam_games_review.csv', index=False, encoding="utf-8-sig")
